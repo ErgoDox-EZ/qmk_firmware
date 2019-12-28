9ac8b7f80c02a5d74a72b21e2843f881096f9fce
Drashna Jael're <drashna@live.com>
2019-12-28 00:36:52 -0800

    Use White channel on RGBW LEDs

--- quantum/rgblight.c
+++ quantum/rgblight.c
@@ -620,6 +620,11 @@ void rgblight_set(void) {
 #    else
     start_led = led + clipping_start_pos;
 #    endif
+#ifdef RGBW
+    for (uint8_t i = 0; i < num_leds; i++) {
+        convert_rgb_to_rgbw(&start_led[i]);
+    }
+#endif
     ws2812_setleds(start_led, num_leds);
 }
 #endif
