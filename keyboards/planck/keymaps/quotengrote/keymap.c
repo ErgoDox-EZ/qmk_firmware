776e476d4d1c73608f29308a9b4faf705c552873
Michael Grote <38253905+quotengrote@users.noreply.github.com>
2020-09-08 10:25:07 +0200

    wake button entfernt

--- keyboards/planck/keymaps/quotengrote/keymap.c
+++ keyboards/planck/keymaps/quotengrote/keymap.c
@@ -31,11 +31,11 @@ enum planck_layers {
 #define RAISE MO(_RAISE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT_planck_grid( KC_ESCAPE             ,KC_Q     ,KC_W   ,KC_E         ,KC_R ,KC_T     ,DE_Z    ,KC_U  ,KC_I     ,KC_O   ,KC_P    ,KC_BSPACE,
-                                KC_TAB,KC_A           ,KC_S     ,KC_D   ,KC_F         ,KC_G ,KC_H     ,KC_J    ,KC_K  ,KC_L     ,DE_PLUS,KC_ENTER,
-                                KC_LSHIFT             ,DE_Y     ,KC_X   ,KC_C         ,KC_V ,KC_B     ,KC_N    ,KC_M  ,KC_COMMA ,KC_DOT ,DE_MINS ,LT(5,DE_HASH),
-                                LCTL_T(KC_SYSTEM_WAKE),KC_LGUI  ,KC_LALT,LT(5,XXXXXXX),TT(1),KC_SPACE ,XXXXXXX ,TT(2) ,KC_LEFT  ,KC_DOWN,KC_UP   ,KC_RIGHT),
-
+  [_BASE] = LAYOUT_planck_grid( KC_ESCAPE ,KC_Q     ,KC_W   ,KC_E         ,KC_R ,KC_T     ,DE_Z    ,KC_U  ,KC_I     ,KC_O   ,KC_P    ,KC_BSPACE,
+                                KC_TAB    ,KC_A     ,KC_S   ,KC_D         ,KC_F ,KC_G     ,KC_H    ,KC_J  ,KC_K     ,KC_L   ,DE_PLUS ,KC_ENTER,
+                                KC_LSHIFT ,DE_Y     ,KC_X   ,KC_C         ,KC_V ,KC_B     ,KC_N    ,KC_M  ,KC_COMMA ,KC_DOT ,DE_MINS ,LT(5,DE_HASH),
+                                KC_LCTRL  ,KC_LGUI  ,KC_LALT,LT(5,XXXXXXX),TT(1),KC_SPACE ,XXXXXXX ,TT(2) ,KC_LEFT  ,KC_DOWN,KC_UP   ,KC_RIGHT),
+     
   [_LOWER] = LAYOUT_planck_grid(_______,KC_F1      ,KC_F2         ,KC_F3      ,KC_F4      ,KC_F5          ,KC_F6                 ,KC_F7     ,KC_F8    ,KC_F9      ,KC_F10 ,_______,
                                 _______,ST_MACRO_0 ,KC_CALCULATOR ,ST_MACRO_1 ,LGUI(KC_M) ,LGUI(KC_L)     ,LALT(LCTL(KC_DELETE)) ,XXXXXXX   ,XXXXXXX  ,KC_F11     ,KC_F12 ,_______,
                                 _______,ST_MACRO_2 ,TT(4)         ,_______    ,KC_INSERT  ,KC_APPLICATION ,XXXXXXX               ,KC_PAUSE  ,_______  ,_______    ,_______,_______,
