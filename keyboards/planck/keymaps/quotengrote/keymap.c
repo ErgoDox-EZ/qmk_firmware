fe6216455582ae72bb13a366048ee8614d3f84e9
Michael Grote <38253905+quotengrote@users.noreply.github.com>
2020-09-17 20:47:50 +0200

    wip

--- keyboards/planck/keymaps/quotengrote/keymap.c
+++ keyboards/planck/keymaps/quotengrote/keymap.c
@@ -30,33 +30,51 @@ enum planck_layers {
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 
-//Tap Dance
-//https://beta.docs.qmk.fm/using-qmk/software-features/feature_tap_dance
 //Deutsche Sonderzeichen definieren
 #define DE_UDIA KC_LBRC // Ü - german.h
 #define DE_ODIA KC_SCLN // Ö - german.h
 #define DE_ADIA KC_QUOT // Ä - german.h
 #define DE_SS   KC_MINS // ß - german.h
-//Tasten deklarieren
-enum {
-    TD_AE,
-    TD_UE,
-    TD_OE,
-    TD_SZ,
-    TD_CT, //Win + Context
-};
-//Definitionen
+#define DE_EURO ALGR(DE_E) // € - german.h
 
-qk_tap_dance_action_t tap_dance_actions[] = {
+enum {
+    TD_AE, //TT ä
+    TD_UE, //TT ü
+    TD_OE, //TT ö
+    TD_SZ, //TT ß
+    TD_CT, //TT Win + Context
+    TDA_E, //TT-ACTION_TAP_DANCE_FN €
+    TDA_A, //TT-ACTION_TAP_DANCE_FN @
+    TDA_P //TT-ACTION_TAP_DANCE_FN |
+};
+
+//Funktion für TT-ACTION_TAP_DANCE_FN €
+void F_TDA_E (qk_tap_dance_state_t *state, void *user_data) {
+	if (state->count <= 2) {
+		register_code(KC_X);
+		unregister_code(KC_X);
+	}
+	else {
+		register_code16(DE_EURO);
+		unregister_code16(DE_EURO);	}
+}
+//Funktion für TT-ACTION_TAP_DANCE_FN @
+//Funktion für TT-ACTION_TAP_DANCE_FN |
+
+
+
+
+
+//TT+TDA-Funktion
+qk_tap_dance_action_t tap_dance_actions[] = { //mit TD(Name) in Keymap einbinden
     [TD_AE] = ACTION_TAP_DANCE_DOUBLE(KC_A, DE_ADIA),
     [TD_UE] = ACTION_TAP_DANCE_DOUBLE(KC_U, DE_UDIA),
     [TD_OE] = ACTION_TAP_DANCE_DOUBLE(KC_O, DE_ODIA),
     [TD_SZ] = ACTION_TAP_DANCE_DOUBLE(KC_S, DE_SS),
     [TD_CT] = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, KC_APPLICATION),
+    [TDA_E] = ACTION_TAP_DANCE_FN(F_TDA_E)
+
 };
-
-//mit TD(Name) in Keymap einbinden
-
 
 
 
@@ -64,7 +82,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT_planck_grid(  KC_ESCAPE  ,KC_Q       ,KC_W           ,KC_E             ,KC_R       ,KC_T         ,DE_Z                 ,TD(TD_UE)      ,KC_I       ,TD(TD_OE)      ,KC_P             ,KC_BSPACE,
+  [_BASE] = LAYOUT_planck_grid(  KC_ESCAPE  ,KC_Q       ,KC_W           ,TDA_E            ,KC_R       ,KC_T         ,DE_Z                 ,TD(TD_UE)      ,KC_I       ,TD(TD_OE)      ,KC_P             ,KC_BSPACE,
                                  KC_TAB     ,TD(TD_AE)  ,TD(TD_SZ)      ,KC_D             ,KC_F       ,KC_G         ,KC_H                 ,KC_J           ,KC_K       ,KC_L           ,DE_PLUS          ,KC_ENTER,
                                  KC_LSHIFT  ,DE_Y       ,KC_X           ,KC_C             ,KC_V       ,KC_B         ,KC_N                 ,KC_M           ,KC_COMMA   ,KC_DOT         ,DE_MINS          ,LT(5,DE_HASH),
                                  KC_LCTRL   ,TD(TD_CT)  ,KC_LALT        ,LT(5,XXXXXXX)    ,TT(1)      ,KC_SPACE     ,XXXXXXX              ,TT(2)          ,KC_LEFT    ,KC_DOWN        ,KC_UP            ,KC_RIGHT),
