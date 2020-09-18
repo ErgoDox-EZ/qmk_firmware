e36e918672f42633dcb2ff1a0fb1f4a776bd9d12
Michael Grote <38253905+quotengrote@users.noreply.github.com>
2020-09-18 22:19:50 +0200

    auto timeout für leds + farbe und muster für startup eingebaut

--- keyboards/planck/keymaps/quotengrote/config.h
+++ keyboards/planck/keymaps/quotengrote/config.h
@@ -8,7 +8,17 @@
 */
 #define ORYX_CONFIGURATOR //auch für led
 #define FIRMWARE_VERSION u8"Ppzmm/554eY"
-#define RGB_MATRIX_STARTUP_SPD 1
 
 //tap dance
 #define TAPPING_TERM 200 //in milliseconds
+
+
+//led startup farbe + muster
+#define RGB_DISABLE_TIMEOUT 600000 // number of milliseconds to wait until rgb automatically turns off = 10min
+#define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_ALPHAS_MODS //Sets the default mode, if none has been set; modus von links, unten und rechts eine farbe haben, und die unneren tasten eine andere/ RGB_MATRIX_ALPHAS_MODS,         // Static dual hue, speed is hue for secondary hue, rgb_spi ist außen, rgb_hui innen
+#define RGB_MATRIX_STARTUP_SAT 254 //Sets the default saturation value, if none has been set
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 220 // Sets the default brightness value, if none has been set
+
+#define RGB_MATRIX_STARTUP_HUE 128 // Sets the default hue value, if none has been set
+#define RGB_MATRIX_STARTUP_SPD 10 // Sets the default animation speed, if none has been set
