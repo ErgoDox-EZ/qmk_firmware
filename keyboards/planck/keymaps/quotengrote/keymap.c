81a9a919296ef475cd3f1022d53914b67161bbbb
Michael Grote <38253905+quotengrote@users.noreply.github.com>
2020-10-01 17:30:28 +0200

    bezeichnungen macros angepast

--- keyboards/planck/keymaps/quotengrote/keymap.c
+++ keyboards/planck/keymaps/quotengrote/keymap.c
@@ -10,8 +10,8 @@ enum planck_keycodes {
   RGB_SLD = EZ_SAFE_RANGE,
   HSV_17_255_211,
   HSV_0_0_255,
-  ST_MACRO_0,
-  ST_MACRO_2,
+  MCR_KA,
+  MCR_KK,
   DE_LSPO,
   DE_RSPC,
 };
@@ -56,8 +56,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  KC_LCTRL   ,KC_LGUI    ,KC_LALT        ,LT(4,KC_APPLICATION) ,TT(1)      ,LT(5,KC_SPACE) ,XXXXXXX              ,TT(2)          ,KC_LEFT    ,KC_DOWN        ,KC_UP            ,KC_RIGHT),
 
   [_LOWER] = LAYOUT_planck_grid( _______    ,KC_F1      ,KC_F2          ,KC_F3                ,KC_F4      ,KC_F5          ,KC_F6                ,KC_F7          ,KC_F8      ,KC_F9          ,KC_F10           ,_______,
-                                 _______    ,ST_MACRO_0 ,KC_CALCULATOR  ,_______              ,XXXXXXX    ,XXXXXXX        ,XXXXXXX              ,XXXXXXX        ,XXXXXXX    ,KC_F11         ,KC_F12           ,_______,
-                                 _______    ,ST_MACRO_2 ,TT(4)          ,_______              ,KC_INSERT  ,XXXXXXX        ,XXXXXXX              ,KC_PAUSE       ,_______    ,_______        ,_______          ,_______,
+                                 _______    ,MCR_KA     ,KC_CALCULATOR  ,_______              ,XXXXXXX    ,XXXXXXX        ,XXXXXXX              ,XXXXXXX        ,XXXXXXX    ,KC_F11         ,KC_F12           ,_______,
+                                 _______    ,MCR_KK     ,TT(4)          ,_______              ,KC_INSERT  ,XXXXXXX        ,XXXXXXX              ,KC_PAUSE       ,_______    ,_______        ,_______          ,_______,
                                  _______    ,_______    ,_______        ,_______              ,_______    ,_______        ,XXXXXXX              ,_______        ,_______    ,KC_PGDOWN      ,KC_PGUP          ,_______),
 
   [_RAISE] = LAYOUT_planck_grid( _______    ,KC_1       ,KC_2           ,KC_3                 ,KC_4       ,KC_5           ,KC_6                 ,KC_7           ,KC_8       ,KC_9           ,KC_0             ,_______,
@@ -166,12 +166,12 @@ void rgb_matrix_indicators_user(void) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case ST_MACRO_0: //keepass strg +a
+    case MCR_KA: //keepass strg +a
       if (record->event.pressed) {
         SEND_STRING(SS_LALT(SS_LCTL(SS_TAP(X_A))));
     }
     break;
-    case ST_MACRO_2: // keepass strg + k
+    case MCR_KK: // keepass strg + k
       if (record->event.pressed) {
         SEND_STRING(SS_LALT(SS_LCTL(SS_TAP(X_K))));
     }
